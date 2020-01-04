e5adb3f0171e6be013bc33a52bddb68a8dc36d60
Lonny Gomes <code@lonnygomes.com>
2020-01-04 12:49:17 -0500

    feat: create mapToEvent to convert passage to ics object

--- src/export.js
+++ src/export.js
@@ -1,9 +1,50 @@
+const moment = require('moment');
+
 class Exporter {
     constructor(passages) {
         if (!passages) {
             throw new Error('passages array must be supplied supplied');
         }
         this.passages = passages;
+    }
+
+    /**
+     * Accepts a passage entry and creates an event object parsable by the ics module
+     * @param {string} title Title for calendar event
+     * @param {number} duration total days for event
+     * @param {object} passage and object containing date, verse, and url items
+     * @returns {object} ics event object
+     */
+    mapToEvent(title, duration, passage) {
+        if (isNaN(duration)) {
+            throw new Error('duration must be supplied as an integer');
+        }
+
+        if (!passage.date) {
+            throw new Error('date item missing in passage');
+        }
+
+        const date = new Date(passage.date);
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+
+        const endDate = moment(passage.date)
+            .add(duration, 'days')
+            .toDate();
+        const endYear = endDate.getFullYear();
+        const endMonth = endDate.getMonth() + 1;
+        const endDay = endDate.getDate();
+
+        const event = {
+            start: [year, month, day],
+            end: [endYear, endMonth, endDay],
+            title,
+            description: passage.verse,
+            url: passage.url
+        };
+
+        return event;
     }
 }
 
--- tests/export.test.js
+++ tests/export.test.js
@@ -14,3 +14,77 @@ describe('constructor', () => {
         );
     });
 });
+
+describe('mapToEvent', () => {
+    test('should throw an error if duration is not supplied', () => {
+        const weekObj = SAMPLE_INPUT[0];
+
+        const passage = Object.assign({}, weekObj.readings[0]);
+        const exporter = new Exporter(SAMPLE_INPUT);
+
+        expect(() => exporter.mapToEvent('title')).toThrow(
+            /duration must be supplied as an integer/
+        );
+    });
+
+    test('should throw an error if duration is not an integer', () => {
+        const weekObj = SAMPLE_INPUT[0];
+
+        const passage = Object.assign({}, weekObj.readings[0]);
+        const exporter = new Exporter(SAMPLE_INPUT);
+
+        expect(() => exporter.mapToEvent('title', '1a', passage)).toThrow(
+            /duration must be supplied as an integer/
+        );
+    });
+
+    test('should throw an error if passage object does not contain a date element', () => {
+        const weekObj = SAMPLE_INPUT[0];
+
+        const passage = Object.assign({}, weekObj.readings[0]);
+        const exporter = new Exporter(SAMPLE_INPUT);
+
+        passage.date = null;
+
+        expect(() => exporter.mapToEvent('title', '1a', passage)).toThrow(
+            /duration must be supplied as an integer/
+        );
+    });
+
+    test('should generate ics-compliant event object', () => {
+        const expectedResult = {
+            start: [2020, 1, 1],
+            end: [2020, 1, 2],
+            title: 'Day 1 Reading',
+            description: 'Psalm 1',
+            url:
+                'https://www.biblegateway.com/passage/?search=Psalm%201&version=ESV'
+        };
+
+        const weekObj = SAMPLE_INPUT[0];
+
+        const passage = Object.assign({}, weekObj.readings[0]);
+        const exporter = new Exporter(SAMPLE_INPUT);
+        const result = exporter.mapToEvent('Day 1 Reading', 1, passage);
+        expect(result).toEqual(expectedResult);
+    });
+
+    test('should generate ics-compliant event object that spans multiple days', () => {
+        const expectedResult = {
+            start: [2020, 1, 1],
+            end: [2020, 1, 8],
+            title: 'Week 1 Memory Verse',
+            description: 'Psalm 101:2',
+            url:
+                'https://www.biblegateway.com/passage/?search=Psalm%20101:2&version=ESV'
+        };
+
+        const weekObj = SAMPLE_INPUT[0];
+
+        const passage = Object.assign({}, weekObj.memoryVerse);
+        passage.date = weekObj.startDate;
+        const exporter = new Exporter(SAMPLE_INPUT);
+        const result = exporter.mapToEvent('Week 1 Memory Verse', 7, passage);
+        expect(result).toEqual(expectedResult);
+    });
+});
